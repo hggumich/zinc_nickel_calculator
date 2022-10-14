133c3a3f3acb7a7c80e759ff85457e82821fc9ce
Hector Garza <hggumich@gmail.com>
2022-10-14 14:25:00 -0400

    add solution type caluclation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
 
 fn main() {
+    
+    let fill_volume = 0.0;
+
     loop {
         println!("This program is for setting up Zinc-Nickel Plating as a");
         println!("low hydrogen embrittlement alkaline electrodeposited");
@@ -30,48 +33,65 @@ fn main() {
             println!("These two products are good for the following specifications:");
             println!("AMS 2417, BAC 5637, MIL-PRF-32660, U.S. Air Force 201027456");
         } else if user_value == 1 {
-            
             println!("\nPlease enter height dimension in inches of the tank: ");
             let mut tank_height = String::new();
-            io::stdin().read_line(&mut tank_height).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut tank_height)
+                .expect("Failed to read line");
             let tank_height: f32 = tank_height.trim().parse().expect("Please type a number!");
-            
+
             println!("Please enter width dimension in inches of the tank: ");
             let mut tank_width = String::new();
-            io::stdin().read_line(&mut tank_width).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut tank_width)
+                .expect("Failed to read line");
             let tank_width: f32 = tank_width.trim().parse().expect("Please type a number!");
 
             println!("Please enter length dimension in inches of the tank: ");
             let mut tank_length = String::new();
-            io::stdin().read_line(&mut tank_length).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut tank_length)
+                .expect("Failed to read line");
             let tank_length: f32 = tank_length.trim().parse().expect("Please type a number!");
 
             println!("Please enter filled depth dimension in inches of the tank: ");
             let mut tank_depth = String::new();
-            io::stdin().read_line(&mut tank_depth).expect("Failed to read line");
+            io::stdin()
+                .read_line(&mut tank_depth)
+                .expect("Failed to read line");
             let tank_depth: f32 = tank_depth.trim().parse().expect("Please type a number!");
 
-            let full_volume: f32 = tank_height*tank_width*tank_length;
-            let less_volume: f32 = (tank_height - tank_depth)*tank_width*tank_length;
-            let tank_volume: f32 = (full_volume-less_volume)* 0.004329;
+            let full_volume: f32 = tank_height * tank_width * tank_length * 0.004329;
+            let less_volume: f32 = (tank_height - tank_depth) * tank_width * tank_length * 0.004329;
+            let fill_volume: f32 = full_volume - less_volume;
 
             println!("{}, {}, {}", tank_height, tank_length, tank_width);
-            println!("Total Tank Capacity volume is {} gallons ", (full_volume*0.004329).floor());
-            println!("Filled Tank volume is {} gallons ", tank_volume.floor());
-
+            println!(
+                "Total Tank Capacity volume is {} gallons ", full_volume.floor()
+            );
+            println!("Filled Tank volume is {} gallons ", fill_volume.floor());
         } else if user_value == 2 {
             println!("Please enter solution: 1 for Dispol IZ-C17 or 2 for Dispol IZ-264");
+            let mut user_chemical = String::new();
+            io::stdin()
+                .read_line(&mut user_chemical)
+                .expect("Failed to read line");
+            let user_chemical: i32 = user_chemical.trim().parse().expect("Please type a number!");
 
-            println!("Require chemicals for Dispol IZ-C17 are as follows: ");
-            println!("Dipsol IZ-C17+MS : # gallons");
-            println!("Dipsol Sodium Hydroxide: # pounds");
-            println!("Dipsol NZ-777: # gallons");
-            println!("Dipsol IZ-C17+B: # gallons");
-            println!("Dipsol F-0529: # gallons");
-
-            println!("Require chemicals for Dispol IZ-264 are as follows: ");
-            println!("Dispol IZ-264: # gallons");
-            println!("Dispol IZ-264 T: # gallons");
+            if user_chemical == 1 {
+                println!("Require chemicals for Dispol IZ-C17 are as follows: ");
+                println!("Dipsol IZ-C17+MS : {} gallons", fill_volume*19.2/100.0);
+                println!("Dipsol Sodium Hydroxide: {} pounds", fill_volume*90.0/100.0);
+                println!("Dipsol NZ-777: {} gallons", fill_volume*1.28/100.0);
+                println!("Dipsol IZ-C17+B: {} gallons", fill_volume*2.1/100.0);
+                println!("Dipsol F-0529: {} gallons", fill_volume*0.4/100.0);
+            } else if user_chemical == 2 {
+                println!("Require chemicals for Dispol IZ-264 are as follows: ");
+                println!("Dispol IZ-264: {} gallons", fill_volume*7.5/100.0);
+                println!("Dispol IZ-264 T: {} gallons", fill_volume*4.0/100.0);
+            } else {
+                println!("You entered an invalid respond!");
+            }
         } else if user_value == 3 {
             println!("Application of Dispol IZ-C17 with Dispol IZ-264 is as follows: ");
             println!("Step 1: Degrease - Solvent Clean to remove grease and oil");
