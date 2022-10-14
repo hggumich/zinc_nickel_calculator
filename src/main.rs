c976f51b8a72d291aec8cb36eb768d71c3461ce2
Hector Garza <hggumich@gmail.com>
2022-10-14 09:17:19 -0400

    add contacts

--- src/main.rs
+++ src/main.rs
@@ -49,4 +49,17 @@ fn main() {
     println!("Step 21: Inspect - measure thickness and nickel co-deposition");
     println!("Step 22: Bake - bake within 4 hours of completion of plating at 375F for 23 hours minimum");
         
+    println!("Dispol of America");
+    println!("34005 Schoolcraft St, Livonia, Mi 48150");
+    println!("phone: 734-261-0633");
+    println!("website: https://www.dipsolamerica.com/");
+    println!("Contacts: ");
+    println!("Chris Kolar");
+    println!("Title: Technical Service Representative");
+    println!("email: ckolar@dipsolamerica.com");
+    println!("phone: 734-812-1021");
+    println!("John Szczypka");
+    println!("Title: NA Sales Manager");
+    println!("email: JSzczypka@dipsolamerica.com");
+    println!("phone: 734-261-0633");
 }
