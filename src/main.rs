093b2e80c1875aa24d3c92fe87f55b45cb4e86b0
Hector Garza <hggumich@gmail.com>
2022-10-14 09:03:21 -0400

    add application of the products onto parts

--- src/main.rs
+++ src/main.rs
@@ -25,4 +25,28 @@ fn main() {
     println!("Dispol IZ-264: # gallons");
     println!("Dispol IZ-264 T: # gallons");
         
+    println!("Application of Dispol IZ-C17 with Dispol IZ-264 is as follows: ");
+    println!("Step 1: Degrease - Solvent Clean to remove grease and oil");
+    println!("Step 2: Grit Blast - 120 grit aluminum oxide at 40 - 60 psi per MIL-STD-1504 or equivalent");
+    println!("Step 3: Clean - remove grit blast residue with clean compressed air. Plate within (1) hour after grit blast");
+    println!("Step 4: Mask - as required");
+    println!("Step 5: Rinse and Clean - water rinse for 90 seconds and scrub part with clean nylon bristle brush");
+    println!("Step 6: Inspect - water-break free surface");
+    println!("Step 7: Acid Activate - immerse part in 0.1% HCl for 30 seconds max or until gassing observed");
+    println!("Step 8: Rinse - water rinse for minimum of 90 seconds");
+    println!("Step 9: Plating - plate at 28 - 56 ASF for 25 minutes to produce 0.3 - 0.6 mils at 68 - 78 F");
+    println!("Step 10: Rinse - water rinse for minimum of 90 seconds");
+    println!("Step 11: Demask - keep product wet");
+    println!("Step 12: Rinse - water rinse for minimum of 30 seconds");
+    println!("Step 13: Acid Activate - immerse in 0.1% HCl for 30 seconds max");
+    println!("Step 14: Rinse - water rinse for minimum of 30 seconds");
+    println!("Step 15: Trivalent Chromate Conversion Coating - Immersion in solution for 30 to 120 seconds at 61 to 89 F");
+    println!("Step 16: Air Time - Hold parts in Air  for 30 seconds to complete Chromate Conversion Reaction");
+    println!("Step 17: Rinse - water rinse for minimum of 90 seconds");
+    println!("Step 18: Rinse - hot water rinse at 160 - 190 F for 10 seconds");
+    println!("Step 19: Drying - apply forced air drying");
+    println!("Step 20: Demask/Unrack - if not performed before");
+    println!("Step 21: Inspect - measure thickness and nickel co-deposition");
+    println!("Step 22: Bake - bake within 4 hours of completion of plating at 375F for 23 hours minimum");
+        
 }
