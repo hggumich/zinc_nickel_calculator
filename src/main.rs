16ff972480b7407185f0b13d1405839ea806804c
Hector Garza <hggumich@gmail.com>
2022-10-14 08:35:46 -0400

    add printout for chemicals and amount

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,23 @@ fn main() {
     println!("Trivalent Chromate Conversion Coating: Dispol IZ-264");
     println!("These two products are good for the following specifications:");
     println!("AMS 2417, BAC 5637, MIL-PRF-32660, U.S. Air Force 201027456");
+
+    println!("Please enter height dimension in inches of the tank: ");
+    println!("Please enter width dimension in inches of the tank: ");
+    println!("Please enter length dimension in inches of the tank: ");
+    println!("Please enter filled depth dimension in inches of the tank: ");
+
+    println!("Please enter solution: 1 for Dispol IZ-C17 or 2 for Dispol IZ-264");
+
+    println!("Require chemicals for Dispol IZ-C17 are as follows: ");
+    println!("Dipsol IZ-C17+MS : # gallons");
+    println!("Dipsol Sodium Hydroxide: # pounds");
+    println!("Dipsol NZ-777: # gallons");
+    println!("Dipsol IZ-C17+B: # gallons");
+    println!("Dipsol F-0529: # gallons");
+
+    println!("Require chemicals for Dispol IZ-264 are as follows: ");
+    println!("Dispol IZ-264: # gallons");
+    println!("Dispol IZ-264 T: # gallons");
+        
 }
