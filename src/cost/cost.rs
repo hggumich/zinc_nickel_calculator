6c89f624f4f3e0e857d0f9854330afef6862d410
Hector Garza <hggumich@gmail.com>
2022-10-18 13:18:11 -0400

    add cost and application modules

---
+++ src/cost/cost.rs
@@ -0,0 +1,18 @@
+pub fn cost() {
+
+    println!("Chemical Cost");
+    println!("Dipsol IZ-C17+ Zinc-Nickel Component Pricing: ");
+    println!("Dipsol IZ-C17+MS: $65.00 per gal");
+    println!("Dipsol IZ-C17+Ni: $103.50 per gal");
+    println!("Dipsol IZ-C17+B: $167.00 per gal");
+    println!("Dipsol NZ-777: $21.66 per gal");
+    println!("Dipsol F-0529: $42.00 per gal");
+    println!("Sodium Hydroxide: $1.86 per pound");
+
+    println!("Dipsol IZ-264 Trivalent Passivate Component Pricing: ");
+    println!("Dipsol IZ-264: $47.00 per gal");
+    println!("Dipsol IZ-264-T: $47.00 per gal");
+
+    println!("Cost for Dipsol IZ-C17+ is: ");
+    println!("Cost for Dipsol IZ-264 is: ");
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod contact;
 mod quality;
 mod maintenance;
 mod equipment;
+mod application;
+mod cost;
 
 fn main() {
     loop {
@@ -21,7 +23,7 @@ fn main() {
         } else if user_value == 2 {
             get_solution_makeup_function();
         } else if user_value == 3 {
-            display_application_function();
+            application::application::application();
         } else if user_value == 4 {
             contact::contact::contact();
         } else if user_value == 5 {
@@ -31,7 +33,7 @@ fn main() {
         } else if user_value == 7 {
             maintenance::maintenance::maintenance();
         } else if user_value == 8 {
-            cost_function();
+            cost::cost::cost();
         } else {
             println!("You entered an invalid respond.");
         }
@@ -48,24 +50,6 @@ fn main() {
         }
     }
 }
-fn cost_function() {
-    println!("Chemical Cost");
-    println!("Dipsol IZ-C17+ Zinc-Nickel Component Pricing: ");
-    println!("Dipsol IZ-C17+MS: $65.00 per gal");
-    println!("Dipsol IZ-C17+Ni: $103.50 per gal");
-    println!("Dipsol IZ-C17+B: $167.00 per gal");
-    println!("Dipsol NZ-777: $21.66 per gal");
-    println!("Dipsol F-0529: $42.00 per gal");
-    println!("Sodium Hydroxide: $1.86 per pound");
-
-    println!("Dipsol IZ-264 Trivalent Passivate Component Pricing: ");
-    println!("Dipsol IZ-264: $47.00 per gal");
-    println!("Dipsol IZ-264-T: $47.00 per gal");
-
-    println!("Cost for Dipsol IZ-C17+ is: ");
-    println!("Cost for Dipsol IZ-264 is: ");
-}
-
 
 fn get_tank_dimension_function() {
     println!("\nPlease enter height dimension in inches of the tank: ");
@@ -136,34 +120,6 @@ fn get_solution_makeup_function() {
     }
 }
 
-fn display_application_function() {
-    println!("Application of Dispol IZ-C17 with Dispol IZ-264 is as follows: ");
-    println!("Step 1: Degrease - Solvent Clean to remove grease and oil");
-    println!("Step 2: Pre Plate Stress Relief - bake for 5 - 10 hours at 375 F");
-    println!("Step 3: Grit Blast - 120 grit aluminum oxide at 40 - 60 psi per MIL-STD-1504 or equivalent");
-    println!("Step 4: Clean - remove grit blast residue with clean compressed air. Plate within (1) hour after grit blast");
-    println!("Step 5: Mask - as required");
-    println!("Step 6: Rinse and Clean - water rinse for 90 seconds and scrub part with clean nylon bristle brush");
-    println!("Step 7: Inspect - water-break free surface");
-    println!("Step 8: Acid Activate - immerse part in 0.1% HCl for 30 seconds max or until gassing observed");
-    println!("Step 9: Rinse - water rinse for minimum of 90 seconds");
-    println!("Step 10: Plating - plate at 28 - 56 ASF for 25 minutes to produce 0.3 - 0.6 mils at 68 - 78 F");
-    println!("Step 11: Rinse - water rinse for minimum of 90 seconds");
-    println!("Step 12: Demask - keep product wet");
-    println!("Step 13: Rinse - water rinse for minimum of 30 seconds");
-    println!("Step 14: Acid Activate - immerse in 0.1% HCl for 30 seconds max");
-    println!("Step 15: Rinse - water rinse for minimum of 30 seconds");
-    println!("Step 16: Trivalent Chromate Conversion Coating - Immersion in solution for 30 to 120 seconds at 61 to 89 F");
-    println!("Step 17: Air Time - Hold parts in Air  for 30 seconds to complete Chromate Conversion Reaction");
-    println!("Step 18: Rinse - water rinse for minimum of 90 seconds");
-    println!("Step 19: Rinse - hot water rinse at 160 - 190 F for 10 seconds");
-    println!("Step 20: Drying - apply forced air drying");
-    println!("Step 21: Demask/Unrack - if not performed before");
-    println!("Step 22: Inspect - measure thickness and nickel co-deposition");
-    println!(
-        "Step 23: Bake - bake within 4 hours of completion of plating at 375F for 23 hours minimum"
-    );
-}
 
 fn intro_function() {
     println!("This program is for setting up Zinc-Nickel Plating as a");
