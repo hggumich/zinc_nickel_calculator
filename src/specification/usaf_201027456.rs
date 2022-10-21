84ba0e23371b597196cce29ca69211801e31f7d8
Hector Garza <hggumich@gmail.com>
2022-10-21 09:13:33 -0400

    add specification requirements and order of precedence

--- src/specification/usaf_201027456.rs
+++ src/specification/usaf_201027456.rs
@@ -4,4 +4,20 @@ pub fn info_spec_print() {
     println!("Trivalent Chromate Conversion Coating: Dispol IZ-264");
     println!("These two products are good for the following specifications:");
     println!("AMS 2417, BAC 5637, MIL-PRF-32660, U.S. Air Force 201027456");
+    println!("Specifications required to meet U.S. Air Force:");
+    println!("ASTM B568 - Measurement of Coating Thickness by X-Ray");
+    println!("ASTM E376 - Measuring Coating Thickness by Magnetic-Field or Eddy Current (Electromagnetic) Examination Methods");
+    println!("Mil-STD-1504 - Abrasive Blasting of Aircraft Components");
+    println!("Mil-STD-865 - Selective, Brush Plating, Electro Deposition");
+    println!("Mil-STD-1916 - DOD Preferred Methods for Acceptance of Product");
+    println!("AMS 2451/9 - Plating, Brush, Zinc-Nickel Low Hydrogen Embrittlement");
+    println!("ASTM F519 - Hydrogen Embrittlement Evaluation of Plating Processes & Service Environments");
+    println!("ASTM B160 - Standard Specification for Nickel Rod and Bar");
+    println!("ASTM D1193 - Standard Specification for Reagent Water");
+    println!("ASTM B117 - Standard Practice for Operating Salt Spray Apparatus");
+    println!("ASTM D3359 - Paint Adhesion Testing");
+    println!("Order of Precedence");
+    println!("Conflict between the text's specification and references:");
+    println!("The text's specification takes precedence");
+    println!("Specification does not supersedes applicable law and regulations unless a specific exemption has been obtained.");
 }
