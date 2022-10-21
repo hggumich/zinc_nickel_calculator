5016e943f4e7942b08c48d5cf7e83480a50c37a3
Hector Garza <hggumich@gmail.com>
2022-10-21 09:41:47 -0400

    update equipment and pricing

--- src/equipment/equipment.rs
+++ src/equipment/equipment.rs
@@ -1,13 +1,16 @@
 pub fn equipment() {
     println!("Equipment Requirements are as follows: ");
-    println!("Tanks - non-metallic such as PVC, CPVC, or polyethylene");
-    println!("Anodes - Nickel 200 anodes have 99.0% nickel");
-    println!("Pumps/Filtration - pump at least 3X turnover per hour, filter elements rated at 20 - 50 micrometers");
-    println!("Carbon Treatment - treatment external to plating tank");
-    println!("Carbonate Removal -  cooling equipment to precipitate the carbonates");
-    println!("Toshi Cell - 500 ml Toshi Cell");
-    println!("Amp-Hour Meter - total accumulated amp-hours to determine additions");
-    println!("Do not use air agitation due to it will cause rapid rise in carbonates");
+    println!("Tanks - 24 X 24 X 18 inches: $ 400");
+    println!("Anodes - 4 X 12 X 0.125 inches: $ 2000");
+    println!("Pumps/Filtration - $ 1000");
+    println!("Carbonate Removal -  Chest Freezer: $ 800");
+    println!("Toshi Cell - 500 ml Toshi Cell: $ 550");
+    println!("Amp-Hour Meter - Mesa West Totalizer: $ 720");
+    println!("Chemical Feeder - Mesa West Mighty Brite: $ 2350 ");
+    println!("Chemical Fedding Pumps - $ 2200");
+    println!("Plumbing - $ 1000");
+    println!("Table - $ 1000");
     println!("Fluke 377 Clamp Meter: $ 750");
     println!("Fischer Technology - Benchtop XRF: $ 28,000");
+    println!("Power Supply - $ 5000");
 }
