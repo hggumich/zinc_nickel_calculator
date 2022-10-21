8133c990efe77ecef8e03b2ebed07175f019305e
Hector Garza <hggumich@gmail.com>
2022-10-21 09:57:48 -0400

    update cost for chemical based on min order

--- src/cost/cost.rs
+++ src/cost/cost.rs
@@ -1,17 +1,18 @@
 pub fn cost() {
+    println!("Mininum order is 5 gallons per chemical");
     println!("Chemical Cost");
     println!("Dipsol IZ-C17+ Zinc-Nickel Component Pricing: ");
-    println!("Dipsol IZ-C17+MS: $65.00 per gal");
-    println!("Dipsol IZ-C17+Ni: $103.50 per gal");
-    println!("Dipsol IZ-C17+B: $167.00 per gal");
-    println!("Dipsol NZ-777: $21.66 per gal");
-    println!("Dipsol F-0529: $42.00 per gal");
-    println!("Sodium Hydroxide: $1.86 per pound");
+    println!("Dipsol IZ-C17+MS: $ 67.10 per 5 gal");
+    println!("Dipsol IZ-C17+Ni: $ 108.10 per 5 gal");
+    println!("Dipsol IZ-C17+B: $ 175.80 per 5 gal");
+    println!("Dipsol NZ-777: $ 21.30 per 5 gal");
+    println!("Dipsol F-0529: $ 42.00 per 5 gal");
+    println!("Sodium Hydroxide: $186 per 100 pound");
 
     println!("Dipsol IZ-264 Trivalent Passivate Component Pricing: ");
-    println!("Dipsol IZ-264: $47.00 per gal");
-    println!("Dipsol IZ-264-T: $47.00 per gal");
+    println!("Dipsol IZ-264: $52.00 per 5 gal");
+    println!("Dipsol IZ-264-T: $52.00 per 5 gal");
 
-    println!("Cost for Dipsol IZ-C17+ is: ");
-    println!("Cost for Dipsol IZ-264 is: ");
+    println!("Cost for Dipsol IZ-C17+ is: $ 600.30");
+    println!("Cost for Dipsol IZ-264 is: $ 104.00");
 }
